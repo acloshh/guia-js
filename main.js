99b1088825253b1372ecb65eb070e7b7a1cf1f03
acloshh <santitornau@gmail.com>
2026-05-03 20:41:44 -0300

    ejercicio-7

--- main.js
+++ main.js
@@ -2,24 +2,11 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-let opcion = "iniciar";
-boton.addEventListener("click", () => {
-opcion = input.value
-switch (opcion) {
-    case "iniciar":
-        mensaje.innerHTML = "Iniciando...";
-        break;
-    case "configuracion":
-        mensaje.innerHTML = "Entrando a la configuracion";
-        break;
-    case "creditos":
-        mensaje.innerHTML = "Mostrando los créditos";
-        break;
-    case "salir":
-        mensaje.innerHTML = "Saliendo...";
-        break;
-    default:
-        mensaje.innerHTML = "Opción no válida. Por favor, ingrese una opción válida.";
-        break;
-};
-})
+boton.addEventListener("click", function () {
+  let textoFinal = "";
+  for (let i = 3; i >= 1; i--) {
+    textoFinal += i + "\n";
+  }
+  textoFinal += "¡Comienza!";
+  mensaje.textContent = textoFinal;
+});

85050031b286f85c8b0718b05a5de076e9114c46
acloshh <santitornau@gmail.com>
2026-05-03 21:19:48 -0300

    ejercicio-16

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-const sumar = (a, b) => {
- return a + b;
+const jugador = {
+ nombre: "Luna",
+ vida: 100,
+ energia: 80,
+ nivel: 1,
+ inventario: ["espada", "poción"]
 };
 
-console.log(sumar(10, 5));
+console.log("Jugador:", jugador,);
+console.log("inventario:", jugador.inventario);

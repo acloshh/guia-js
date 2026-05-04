873ab38d891e855073c959667dd708577ca534e8
acloshh <santitornau@gmail.com>
2026-05-03 21:49:02 -0300

    ejercicio-24

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-
+let puntos = 0;
 boton.addEventListener("click", function() {
     const valor = input.value;
-    mensaje.textContent = "Bienvenido a la guía de JavaScript";
-    mensaje.style.color = "blue";
+    mensaje.textContent = puntos+10;
 
 });
 

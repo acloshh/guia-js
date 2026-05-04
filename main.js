ab64d8604c90e89697ca00fb939fe22c35c52867
acloshh <santitornau@gmail.com>
2026-05-03 21:13:48 -0300

    ejercicio-13-ayuda-de-copilot

--- main.js
+++ main.js
@@ -2,9 +2,7 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-const inventario = ["espada", "pocion", "escudo", "mapa", "llave"];
-  if (inventario.includes("llave")) {
-    console.log("La puerta se abre");
-  } else {
-    console.log("Necesitas una llave");
-  }
+function saludarUsuario(nombre) {
+    console.log(`Hola, ${nombre}!`);
+}
+saludarUsuario("Milo");

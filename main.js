10b65ff39003cf219e94887b3982bf6dce7efb39
acloshh <santitornau@gmail.com>
2026-05-03 22:00:45 -0300

    ejercicio-26

--- main.js
+++ main.js
@@ -2,15 +2,15 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-boton.addEventListener("click", function() {
-  const valor = input.value;
-  if (valor == "") {
-    mensaje.textContent = "Ingrese un nombre.";
-  } else {
-    mensaje.textContent = "Bienvenido, " + valor;
-  }
+const jugador = {
+ nombre: "Luna",
+ puntaje: 1500
+};
 
-});
+localStorage.setItem("jugador", JSON.stringify(jugador));
 
+const jugadorGuardado = JSON.parse(localStorage.getItem("jugador"));
+
+console.log("Jugador guardado:", jugadorGuardado);
   
 

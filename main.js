7734f1bf581ab26c6cf39f6c5299c0887bc29f24
acloshh <santitornau@gmail.com>
2026-05-03 21:17:30 -0300

    ejercicio-15

--- main.js
+++ main.js
@@ -2,9 +2,8 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-function calcularVidaRestante(vida, danio) {
- return vida - danio;
-}
+const sumar = (a, b) => {
+ return a + b;
+};
 
-let resultado = calcularVidaRestante(100, 30);
-console.log(resultado);
+console.log(sumar(10, 5));

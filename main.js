431536207a1433a4e8644d5dec62dbca88d994f6
acloshh <santitornau@gmail.com>
2026-05-03 21:35:20 -0300

    ejercicio-19

--- main.js
+++ main.js
@@ -2,12 +2,7 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-const personajes1 = [
- { nombre: "Luna", vida: 100, tipo: "maga", nivel: 3 },
- { nombre: "Kai", vida: 120, tipo: "guerrero", nivel: 4 },
- { nombre: "Nina", vida: 90, tipo: "arquera", nivel: 2 }
-];
-for (let i = 0; i < personajes1.length; i++) {
- const personaje = personajes1[i];
- console.log(`Nombre: ${personaje.nombre}, Vida: ${personaje.vida}, Tipo: ${personaje.tipo}, Nivel: ${personaje.nivel}`);
-}
+const personajesFuertes = personajes.filter((personaje) => {
+ return personaje.nivel>= 3;
+});
+console.log("Personajes con nivel 3 o superior:", personajesFuertes);

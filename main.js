c60c698c731806fb1407c62c8b12829f4a47a542
acloshh <santitornau@gmail.com>
2026-05-03 21:36:12 -0300

    ejercicio-20

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-const personajesFuertes = personajes.filter((personaje) => {
- return personaje.nivel>= 3;
+const nombres = personajes.map((personaje) => {
+ return personaje.nombre;
 });
-console.log("Personajes con nivel 3 o superior:", personajesFuertes);
+
+console.log(nombres);

c689a263e2c2f4105b8f20e7d870a47179bb6469
acloshh <santitornau@gmail.com>
2026-05-03 21:32:14 -0300

    ejercicio-18

--- main.js
+++ main.js
@@ -2,12 +2,12 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-const jugador = {
- nombre: "Kai",
- nivel: 1,
- monedas: 0
-};
-console.log("El jugador " + jugador.nombre + " ha comenzado el juego con nivel " + jugador.nivel + " y " + jugador.monedas + " monedas.");
-console.log("El nivel del jugador aumenta: " + (jugador.nivel = jugador.nivel + 1));
-console.log("El jugador gana monedas: " + (jugador.monedas = jugador.monedas + 50));
-console.log("El jugador " + jugador.nombre + " ahora tiene nivel " + jugador.nivel + " y " + jugador.monedas + " monedas.");
+const personajes1 = [
+ { nombre: "Luna", vida: 100, tipo: "maga", nivel: 3 },
+ { nombre: "Kai", vida: 120, tipo: "guerrero", nivel: 4 },
+ { nombre: "Nina", vida: 90, tipo: "arquera", nivel: 2 }
+];
+for (let i = 0; i < personajes1.length; i++) {
+ const personaje = personajes1[i];
+ console.log(`Nombre: ${personaje.nombre}, Vida: ${personaje.vida}, Tipo: ${personaje.tipo}, Nivel: ${personaje.nivel}`);
+}

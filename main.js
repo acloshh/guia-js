aa97f84430bb2bb1ef8871780511d6cba901276e
acloshh <santitornau@gmail.com>
2026-05-03 21:57:38 -0300

    ejercicio-25

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ const input = document.getElementById("dato");
 const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
-let puntos = 0;
 boton.addEventListener("click", function() {
-    const valor = input.value;
-    mensaje.textContent = puntos+10;
-    puntos += 10;
+  const valor = input.value;
+  if (valor == "") {
+    mensaje.textContent = "Ingrese un nombre.";
+  } else {
+    mensaje.textContent = "Bienvenido, " + valor;
+  }
 
 });
 

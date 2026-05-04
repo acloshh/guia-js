e92a55473480b516ee50fa63e1c9b8481ec7f8b4
acloshh <santitornau@gmail.com>
2026-05-03 21:24:10 -0300

    ejercicio-17

--- main.js
+++ main.js
@@ -3,12 +3,11 @@ const boton = document.getElementById("btn");
 const mensaje = document.getElementById("mensaje");
 
 const jugador = {
- nombre: "Luna",
- vida: 100,
- energia: 80,
+ nombre: "Kai",
  nivel: 1,
- inventario: ["espada", "poción"]
+ monedas: 0
 };
-
-console.log("Jugador:", jugador,);
-console.log("inventario:", jugador.inventario);
+console.log("El jugador " + jugador.nombre + " ha comenzado el juego con nivel " + jugador.nivel + " y " + jugador.monedas + " monedas.");
+console.log("El nivel del jugador aumenta: " + (jugador.nivel = jugador.nivel + 1));
+console.log("El jugador gana monedas: " + (jugador.monedas = jugador.monedas + 50));
+console.log("El jugador " + jugador.nombre + " ahora tiene nivel " + jugador.nivel + " y " + jugador.monedas + " monedas.");
